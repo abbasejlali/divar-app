4d745a6f9c5cc6d5aa230130ebc3d239a00fe15b
abbas ejlali <abbas.ejlali@iran.ir>
2024-11-07 01:47:55 -0800

    start divar app

---
+++ app/utils/shabnamFont.ts
@@ -0,0 +1,56 @@
+import localFont from "next/font/local";
+
+export const ShabnamFont = localFont({
+  src: [
+    {
+      path: "../fonts/Shabnam-Thin-FD.woff",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Shabnam-Thin-FD.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Shabnam-Light-FD.woff",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Shabnam-Light-FD.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Shabnam-FD.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Shabnam-FD.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Shabnam-Medium-FD.woff",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Shabnam-Medium-FD.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Shabnam-Bold-FD.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Shabnam-Bold-FD.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
