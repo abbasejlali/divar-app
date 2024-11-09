138293cbc388c44e2ffe9816ac8bc0b2f6b06b05
abbas ejlali <abbas.ejlali@iran.ir>
2024-11-09 03:52:29 -0800

    build btn menu and responsive menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ShabnamFont } from "@/utils/shabnamFont";
+import LayoutPage from "@/components/layout/LayoutPage";
 
 export const metadata: Metadata = {
   title: "دیوار | عباس اجلالی",
@@ -14,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl">
-      <body className={`${ShabnamFont.className} antialiased`}>{children}</body>
+      <body className={`${ShabnamFont.className} antialiased`}>
+        <LayoutPage>{children}</LayoutPage>
+      </body>
     </html>
   );
 }
