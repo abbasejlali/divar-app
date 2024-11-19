925fd42e592abf8c596b771c030ae332d8838191
abbas ejlali <abbas.ejlali@iran.ir>
2024-11-19 08:24:56 -0800

    add max-width to form add ads

--- app/components/template/AddAdsPage.tsx
+++ app/components/template/AddAdsPage.tsx
@@ -51,7 +51,10 @@ function AddAdsPage() {
   };
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)} className="max-w-sm mx-auto">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="max-w-xl w-full mx-auto"
+      >
         <div className="mb-5">
           <label
             htmlFor="title"
@@ -216,7 +219,7 @@ function AddAdsPage() {
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Submit
+          ثبت آگهی
         </button>
       </form>
     </>
